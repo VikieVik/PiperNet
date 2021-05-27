1dccd75b8d6f9954a15d2bf90211baa37c10a848
Singh-Vikas-M <vikas3091999@gmail.com>
2021-05-28 04:47:16 +0530

    added ble buttons still need to resolve table issue

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -6,42 +6,104 @@
 import React, { useState, useContext, useEffect } from "react";
 import "./Events.css";
 import Sidenav from "../../Components/Sidenav/Sidenav.js";
-import { Table, Tabs } from "antd";
+import { Table, Tabs, Button } from "antd";
+import { ThunderboltOutlined } from "@ant-design/icons";
+import db, { showPayload } from "../../Components/db";
+// BLE control functions
+import {
+  sendToBleDevice,
+  connectToBleDevice,
+  startReadingBleDevice,
+  stopReadingBleDevice,
+} from "../../Components/BLE";
 
 function Events() {
-  const [eventCount, setEventCount] = useState(0);
-  const [eventList, setEventList] = useState();
+  const [dataList, setDataList] = useState();
+  const [bleButtonLabel, setBleButtonLabel] = useState("Start");
 
   const { TabPane } = Tabs;
 
-  function callback(key) {
-    console.log(key);
-  }
-
   const columns = [
     {
-      title: "Event",
-      dataIndex: "event",
+      title: "Gateway",
+      dataIndex: "papaId",
+      key: "papaId",
     },
     {
-      title: "User",
-      dataIndex: "userid",
+      title: "Hotsopt",
+      dataIndex: "duckId",
+      key: "duckId",
     },
     {
-      title: "Data",
-      dataIndex: "properties",
+      title: "Path",
+      dataIndex: "path",
+      key: "path",
+    },
+
+    {
+      title: "Payload",
+      dataIndex: "payload",
+      key: "payload",
     },
     {
-      title: "Time",
-      dataIndex: "timestamp",
+      title: "Message ID",
+      dataIndex: "messageId",
+      key: "messageId",
     },
   ];
 
-  useEffect(() => {}, []);
+  const dumyData = [
+    {
+      papaId: "VIKAS_DUCK",
+      duckId: "MAMA0001",
+      path: "MAMA0001PAPADUCK",
+      payload: "food supplies need at 221B baker street",
+      messageId: 0,
+    },
+  ];
+
+  const handleBLEStartStop = () => {
+    if (bleButtonLabel === "Start") {
+      setBleButtonLabel("Stop");
+      startReadingBleDevice();
+    } else {
+      setBleButtonLabel("Start");
+      stopReadingBleDevice();
+    }
+  };
 
   function onChange(pagination, filters, sorter, extra) {
     console.log("params", pagination, filters, sorter, extra);
   }
+
+  useEffect(() => {}, []);
+
+  function dataStatus() {
+    // setDataList([]);
+    db.allDocs({
+      include_docs: true,
+      descending: true,
+    })
+      .then(function (result) {
+        let datas = result.rows;
+        var dataListTemp = [];
+        for (let id in datas) {
+          dataListTemp.push(datas[id].doc.title);
+          // console.log(datas[id].doc.title);
+        }
+        console.log(dataListTemp);
+        setDataList(dataListTemp);
+        console.log(dataList);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  db.changes({
+    since: "now",
+    live: true,
+  }).on("change", dataStatus);
 
   return (
     <div className="Events">
@@ -49,30 +111,54 @@ function Events() {
         <Sidenav />
         <div className="content-container">
           <div className="content">
-            <div className="header">
-              <h1>Events</h1>
-              <p>See events being sent to this project in near real time.</p>
-            </div>
+            <h1 className="header-h1">Events</h1>
+            <p className="header-p">
+              See events being sent to this project in near real time.
+            </p>
 
-            <Tabs defaultActiveKey="1" onChange={callback} size="large">
-              <TabPane tab="Online-DB" key="1">
+            <Tabs defaultActiveKey="1" size="large">
+              <TabPane tab="Offline-BLE" key="1">
+                <p>Connect to local pipernet gateway over Wired USB</p>
+                <Button
+                  type="primary"
+                  icon={<ThunderboltOutlined />}
+                  style={{ margin: "0px 0px 15px 0px" }}
+                  onClick={connectToBleDevice}
+                >
+                  Connect
+                </Button>
+                <Button
+                  style={{ margin: "0px 0px 15px 15px" }}
+                  onClick={handleBLEStartStop}
+                >
+                  {bleButtonLabel}
+                </Button>
+
+                <Table
+                  className="event-table"
+                  columns={columns}
+                  dataSource={dataList}
+                  onChange={onChange}
+                  size="default"
+                  style={{
+                    boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.8)",
+                  }}
+                  bordered={true}
+                />
+              </TabPane>
+
+              <TabPane tab="Online-DB" key="2">
                 <p>Fetch realtime data from hosted DB over internet</p>
                 <Table
                   className="event-table"
                   columns={columns}
-                  dataSource={eventList}
+                  dataSource={dataList}
                   onChange={onChange}
                   size="default"
-                />
-              </TabPane>
-              <TabPane tab="Offline-BLE" key="2">
-                <p>Connect to local pipernet gateway over BLE</p>
-                <Table
-                  className="event-table"
-                  columns={columns}
-                  dataSource={eventList}
-                  onChange={onChange}
-                  size="default"
+                  style={{
+                    boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.8)",
+                  }}
+                  bordered={true}
                 />
               </TabPane>
             </Tabs>
